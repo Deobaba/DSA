70730a5cef013c07df5bdca574d35987f5cba487
Deobaba <adexsquare4192@gmail.com>
2023-08-01 08:19:00 +0100

    31st done

--- 31-07-23.js
+++ 31-07-23.js
@@ -7,9 +7,19 @@
 // Output: false
 
 
-// function containDuplicate (nums) {
-    
-// }
+function containDuplicate (nums) {
+    let numObject = {}
+    for (let i = 0; i < nums.length; i++){
+        if(Object.hasOwn(numObject,nums[i])){
+          return  console.log(true)
+           
+        }
+    //    else if(numObject.hasOwnProperty(i)){}
+    numObject[nums[i]] = 1
+    }
+    return console.log(false)
+}
 
 
-// containDuplicate('234')
+containDuplicate([1,2,1,3])
+containDuplicate([1,2,3])
